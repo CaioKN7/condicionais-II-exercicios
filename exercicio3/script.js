6a9794c1c37760be1de39b62403420a62bd73a46
CaioKN7 <galdinocaiost@gmail.com>
2023-08-03 20:47:24 -0300

    Exercício 3 condicionais II

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,7 +1,7 @@
 //códigos a serem reescritos
 
 // a)
-if(nome === "José"){
+/* if(nome === "José"){
 	console.log("Oi, Zé!")
 } else {
 	console.log("Olá, " + nome)
@@ -14,3 +14,12 @@ if(idade >= 18){
 } else {
 	console.log("Ainda não pode tirar carteira de motorista!")
 }
+ */
+//const nome = prompt("Me diga seu nome")
+const nome = "José"
+nome === "José" ? console.log("Oi, Zé!"):console.log("Olá, " + nome)
+
+//const idade = +prompt("Me diga sua idade")
+const idade = 18
+
+idade >= 18 ? console.log("Pode tirar carteira de motorista!"):console.log("Ainda não pode tirar carteira de motorista!")
