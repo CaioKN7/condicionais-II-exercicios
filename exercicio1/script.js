771364812a914848c4448a622162a71f6d33c5bb
CaioKN7 <galdinocaiost@gmail.com>
2023-08-03 20:37:21 -0300

    Exercício 2 de condicionais II

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,6 +1,6 @@
 //exercício 1
 
- const numero = +prompt("Digite um número:");
+/*  const numero = +prompt("Digite um número:");
 
  if (numero % 2 === 0) {
     if (numero % 3 === 0) {
@@ -15,7 +15,7 @@
 }
 
 
-/* if (numero % 2 === 0 && numero % 3 === 0) {
+ if (numero % 2 === 0 && numero % 3 === 0) {
     alert("O número é divisível por 2 e por 3.");
 } else if (numero % 2 === 0) {
     alert("O número é divisível apenas por 2.");
@@ -23,4 +23,4 @@
     alert("O número é divisível apenas por 3.");
 } else {
     alert("O número não é divisível por 2 nem por 3.");
-}  */
+} */
